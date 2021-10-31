8fa3f9cb8af375663d781cabd4476da4504c8a43
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:50:59 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/com/semanticcms/view/what_links_here/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/view/what_links_here/book/ModularApidocsRedirect.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-view-what-links-here.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-view-what-links-here.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.semanticcms.view.what_links_here.book;
 
