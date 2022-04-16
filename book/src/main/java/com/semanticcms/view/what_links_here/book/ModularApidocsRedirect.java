fee0ece62e7ca6e4fd94b539b091094131fd05e0
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:49 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/view/what_links_here/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/view/what_links_here/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-view-what-links-here - SemanticCMS view of which pages and elements link to the current page.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-view-what-links-here.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.view.what_links_here.book;
 
 import com.aoapps.net.URIParametersUtils;
